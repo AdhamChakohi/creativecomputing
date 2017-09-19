c68d5952aca6439cf8cefa3c9d881e0f4ad565e0
AdhamChakohi <amc1150@nyu.edu>
2017-09-19 17:27:19 -0400

    buzzer v2

--- buzzer.ino
+++ buzzer.ino
@@ -5,14 +5,12 @@ int pressCounter = 0;
 
 
 void setup() {
-  // put your setup code here, to run once:
   Serial.begin(9600);
   pinMode(buttonPin, INPUT);
   pinMode(buzzerPin, OUTPUT);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
   state = digitalRead(buttonPin);
   Serial.println(state);
   if (state) {
